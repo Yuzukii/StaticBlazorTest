0b9da1b5b60570f14b72b8bf48da32c00a447706
Yuzukii <kelvinleong96@gmail.com>
2021-12-29 17:13:27 +0800

    initial commit

--- Api/WeatherForecastFunction.cs
+++ Api/WeatherForecastFunction.cs
@@ -7,6 +7,9 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 
 using BlazorApp.Shared;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace BlazorApp.Api
 {
@@ -32,6 +35,46 @@ namespace BlazorApp.Api
             return summary;
         }
 
+        private static string GetRiskPot(int snowLvl)
+        {
+            var riskPot = "Low";
+
+            if (snowLvl >= 8)
+            {
+                riskPot = "High";
+            }
+            else if (snowLvl >= 4 && snowLvl <= 7)
+            {
+                riskPot = "Medium";
+            }
+            else if (snowLvl <= 3 )
+            {
+                riskPot = "Low";
+            }
+
+            return riskPot;
+        }
+
+        private static string GetRecommendation(string riskPot)
+        {
+            var recommendation = "";
+
+            if (riskPot == "Low")
+            {
+                recommendation = "Xue Hua Piao Piao, Bei Feng Xiao Xiao";
+            }
+            else if (riskPot == "Medium")
+            {
+                recommendation = "Might wanna bring a shovel along mate";
+            }
+            else if (riskPot == "High")
+            {
+                recommendation = "Stay indoors as if it's corona season.";
+            }
+
+            return recommendation;
+        }
+
         [FunctionName("WeatherForecast")]
         public static IActionResult Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
@@ -48,6 +91,49 @@ namespace BlazorApp.Api
             }).ToArray();
 
             return new OkObjectResult(result);
+        }
+
+        [FunctionName("SnowLevel")]
+        public static IActionResult Run2(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var randomNumber = new Random();
+            var snowLvl = 0;
+            var riskPot = "";
+
+            var result = Enumerable.Range(1, 1).Select(index => new SnowAlert
+            {
+                SnowLvl = snowLvl = randomNumber.Next(0, 10),
+                RiskPot = riskPot = GetRiskPot(snowLvl),
+                Recommendation = GetRecommendation(riskPot)
+            }).ToArray();
+
+            return new OkObjectResult(result);
+        }
+
+
+        [FunctionName("HttpExample")]
+        public static IActionResult Run3(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            //log.LogInformation("C# HTTP trigger function processed a request.");
+
+            //string name = req.Query["name"];
+
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+
+            //string responseMessage = string.IsNullOrEmpty(name)
+            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            string responseMessage = "Hello from the API :P";
+
+
+            return new OkObjectResult(new string("Got issue siah"));
         }
     }
 }
---
+++ Shared/SnowAlert.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared
+{
+    public class SnowAlert
+    {
+        public int SnowLvl { get; set; }
+        public string RiskPot { get; set; }
+        public string Recommendation { get; set; }
+    }
+}
